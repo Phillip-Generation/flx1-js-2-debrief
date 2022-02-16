cafb63f3c150b808ee233c51fbc71fa826e7ff52
flx1derrick <95151579+flx1derrick@users.noreply.github.com>
2022-02-16 13:15:10 -0600

    add genBuzz unfinished

--- app.js
+++ app.js
@@ -25,3 +25,30 @@ const selectExcercises2 = (numberOfExcercises) => {
   
 }
 selectExcercises2(4);
+
+// Part 2 GenBuzz 
+
+let upperLimit = prompt("Please enter a whole number")
+upperLimit = parseInt(upperLimit);
+  
+
+// Check myNumber is type Number
+if (typeof upperLimit !== 'number') {
+  console.log('This is not a number');
+}
+// Check myNumber factor 3
+else if (upperLimit % upperLimit === 0) {
+  console.log('Gen');
+}
+// Check myNumber factor 5
+else if (upperLimit % 5 === 0) {
+  console.log('Buzz');
+}
+// Check myNumber factors of 3 and 5 
+else if (typeof myNumber === 'number' && modThree && modFive) {
+  console.log('GenBuzz');
+
+}
+// Print myNumber if not either factor 3 or 5 
+else
+  console.log(myNumber);
