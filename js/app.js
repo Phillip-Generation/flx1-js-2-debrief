2ad8c94eb6634af84140ed1d3e8327f8bbd1be35
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-06-30 18:50:21 -0400

    Update directions

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ console.log('Your JS file is linked!');
 
 
 
-//3. Print a random integer (whole number) between 50(inclusive) and 100(exclusive) to the console
+//3. Print a random integer
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 //Write your code below this line:
